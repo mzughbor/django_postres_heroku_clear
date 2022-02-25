c4169d64d68a2a295de2e68f71034e79e42a656b
Mahmoud Zuhgbor <mzughbor@gmail.com>
2022-02-25 12:46:49 +0200

    Friday

--- main_02.py
+++ main_02.py
@@ -1,24 +1,30 @@
 import os
+import sys
+import time
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "telegramBot.settings")
 import django
 django.setup()
 from mainbot import views
 from telegram.update import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, Dispatcher
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, Dispatcher, \
+    CallbackQueryHandler
 from telegram.replykeyboardmarkup import ReplyKeyboardMarkup
 from telegram.replykeyboardremove import ReplyKeyboardRemove
 from telegram.bot import Bot
 
-bot = Bot("5135627916:AAHN1isdHyJR9VpeuVvCIbGQInrCtoeA-WQ")
-print(bot.get_me())
+global number_counter
+global word_entered
+
 # updater = Updater("5275565416:AAHLyoqmbpLiUtniz2BnBXKMP_v80aBXGus", use_context=True)
 updater = Updater("5135627916:AAHN1isdHyJR9VpeuVvCIbGQInrCtoeA-WQ", use_context=True)
 from mainbot.models import Post, Books
-
 dispatcher: Dispatcher = updater.dispatcher
 
 
-def start_run(update: Update, context: CallbackContext):
+def start(update: Update, context: CallbackContext):
     """
     the callback for handling start command and create keyboard
     """
@@ -28,7 +34,7 @@ def start_run(update: Update, context: CallbackContext):
                   ["إبحث عن كتاب"]]
     # حكم وأمثال لاغي
     kbd = ReplyKeyboardMarkup(kbd_layout)
-    update.message.reply_text(text="Select Options", reply_markup=kbd)
+    update.message.reply_text(text="من فضلك اختر خياراً من القائمة", reply_markup=kbd)
 
 
 def remove(update: Update, context: CallbackContext):
@@ -41,35 +47,105 @@ def remove(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
-    """
-    message to handle any "Option [0-9]" Regrex.
-    """
-    print("before")
-    if str(update.message.text) == "إبحث عن كتاب":
-        # print(views.Books.objects.order_by('name')[0].description)
-        # update.message.reply_text("You just clicked on '%s'" % views.Books.objects.filter(name__startswith='رواية'))
-        update.message.reply_text("You just clicked on '%s'" % views.Books.objects.filter(name__contains='رواية'))
-        print('ok')
-        # print([e.name for e in Books.objects.all()])
-        print([e.name for e in Books.objects.filter(name__contains='رواية')])
-        print('ok')
-        update.message.reply_text("اسم الكتاب : ... '%s'" % views.Books.objects.filter(name__contains='رواية')[0])
-        update.message.reply_text("الوصف ... '%s'" % views.Books.objects.filter(name__contains='رواية')[0].description)
 
+    if str(update.message.text) == "إبحث عن كتاب":
+        update.message.reply_text("من فضلك اكتب الامر'/name' ثم قم بادخال اسم الكتاب الذي تريده او اسماً مشابها لما تتذكر.. ")
+    pass
+
+
+def name_book(update: Update, context: CallbackContext):
+    """
+    function to know what the hell user of the bot searching for  name of book to read ?.
+    """
+    if update.message.text == '/name':
+        update.message.reply_text("من فضلك أدخل /name أولا ثم الاسم لكي أستطيع مساعدتك !!")
+        time.sleep(2)
+    else:
+        a_string = str(update.message.text)
+        sliced = a_string[5:]
+        # print([e.name for e in Books.objects.all()])
+        after_tra = str(sliced.strip())
+        global word_entered
+        word_entered = after_tra
+        also = [e.name for e in Books.objects.filter(name__contains=after_tra)]  # here where the query happens...
+        if len(also) == 0:
+            update.message.reply_text("نأسف لا يوجد نتائج بحث مطابقة لدينا, حاول بكتاب اخر...")
+            update.message.reply_text("لقد أردت البحث عن ... '%s' " % also)
+        else:
+            keyboard = [[]]
+            counter = 0
+            for x in also:
+                counter += 1
+                update.message.reply_text("نتيجه البحث هي :'%s' " % x)
+                keyboard[0].append(InlineKeyboardButton(x, callback_data=counter))
+
+            if len(also) >= 2:
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+            if len(also) == 1:
+                global number_counter
+                number_counter = len(also)
+                #update.message.reply_text.update_user("اسم الكتاب : ... '%s'" % views.Books.objects.filter(name__contains=after_tra)[0])
+                update.message.reply_text("المؤلف  '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].author)
+                try:
+                    update.message.reply_text("الوصف  '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].description)
+                except:
+                    print("Oops!", sys.exc_info()[0], "occurred.")
+
+                update.message.reply_text("تصنيف الكتاب ... '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].field)
+                update.message.reply_text("اللغة ... '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].language)
+                update.message.reply_text("عدد الصفحات '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].pages)
+                update.message.reply_text("رابط التحميل '%s'" % views.Books.objects.filter(name__contains=after_tra)[0].download_link)
+
+                #if not [e.name for e in Books.objects.filter(name__contains=sliced)]:
+                #print(views.Books.objects.order_by('name')) # description
+                #update.message.reply_text("You just clicked on '%s'" % views.Books.objects.filter(name__startswith='رواية'))
+
+
+def button(update, context):
+    """
+    callback method handling button press
+    """
+    query: CallbackQuery = update.callback_query
+    query.answer()
+    query.edit_message_text(text="لقد اخترت خيار : {}".format(query.data))
+    global i
+    i = int(format(query.data))
+    # also = [e.name for e in Books.objects.filter(name__contains=word_entered)]  # here where the query happens...
+
+
+def multapil_cho(update: Update, context: CallbackContext):
+    after_tra = 'رواية'
+    global i
+    global word_entered
+    word_entered = after_tra
+    also = [e.name for e in Books.objects.filter(name__contains=after_tra)]  # here where the query happens...
+    update.message.reply_text("اسم الكتاب : ... '%s'" % views.Books.objects.filter(name__contains=word_entered)[i])
     pass
 
 
 def randomBooks(update: Update, context: CallbackContext):
-    update.message.reply_text("You just clicked on '%s'" % update.message.text)
+    update.message.reply_text("لقد ضغطت على زر  '%s'" % update.message.text)
     print(update.message.text)
     pass
 
 
-dispatcher.add_handler(CommandHandler("start_run", start_run))
-updater.dispatcher.add_handler(CommandHandler("start_run", start_run))
+def error(update: Update, context: CallbackContext):
+    """Log Errors caused by Updates."""
+    sys.stderr.write("ERROR: '%s' caused by '%s'" % context.error, update)
+    pass
+
+
+dispatcher.add_handler(CommandHandler("start", start))
+updater.dispatcher.add_handler(CommandHandler("start", start))
 updater.dispatcher.add_handler(CommandHandler("remove", remove))
 updater.dispatcher.add_handler(MessageHandler(Filters.regex(r"إبحث عن كتاب"), echo))
+updater.dispatcher.add_handler(CommandHandler("name", name_book))
+#updater.dispatcher.add_handler(CommandHandler("y", multapil_cho))
 updater.dispatcher.add_handler(MessageHandler(Filters.regex(r"كتب عشوائية"), randomBooks))
+updater.dispatcher.add_error_handler(error)  # error handling
+updater.dispatcher.add_handler(CallbackQueryHandler(button))  # handling inline buttons pressing
 
 updater.start_polling()
 

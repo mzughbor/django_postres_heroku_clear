61e76ea468cfb2e07dea0c80e7f7ab94d288ca4a
Mahmoud Zuhgbor <mzughbor@gmail.com>
2022-02-26 23:20:54 +0200

    finally serch is done.

--- main_02.py
+++ main_02.py
@@ -19,8 +19,8 @@ number_counter = 0
 word_entered = ''
 i = 0
 
-# updater = Updater("5275565416:AAHLyoqmbpLiUtniz2BnBXKMP_v80aBXGus", use_context=True)
-updater = Updater("5135627916:AAHN1isdHyJR9VpeuVvCIbGQInrCtoeA-WQ", use_context=True)
+updater = Updater("5275565416:AAHLyoqmbpLiUtniz2BnBXKMP_v80aBXGus", use_context=True)
+# updater = Updater("5135627916:AAHN1isdHyJR9VpeuVvCIbGQInrCtoeA-WQ", use_context=True)
 from mainbot.models import Post, Books
 dispatcher: Dispatcher = updater.dispatcher
 
